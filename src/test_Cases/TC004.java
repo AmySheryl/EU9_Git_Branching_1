1ab5ccaf9d74d65c6d196477878fc5b2799c1663
Amy Sheryl <amysheryl00@gmail.com>
2022-09-13 13:31:36 +0300

    created my own class tc 4 other is accidently happened

---
+++ src/test_Cases/TC004.java
@@ -0,0 +1,15 @@
+package test_Cases;
+
+public class TC004 {
+
+    public static void main(String[] args) {
+
+        // i mistakenly did something wrong
+
+        // this is why I did here again
+
+        // normally I don't need to do this
+
+        // but I have to...
+    }
+}
